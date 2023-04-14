c68be6f73917cadc4e4b92bcbcaa1783264171f3
shreesh30 <shreesharya20@gmail.com>
2023-04-15 03:43:24 +0530

    added rest of the props

--- src/Button.js
+++ src/Button.js
@@ -9,23 +9,32 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
-  const classes = classNames("flex items-center px-3 py-1.5 border", {
-    "border-blue-500 bg-blue-500 text-white": primary,
-    "border-gray-900 bg-gray-900 text-white": secondary,
-    "border-green-500 bg-green-500 text-white": success,
-    "border-yellow-400 bg-yellow-400 text-white": warning,
-    "border-red-500 bg-red-500 text-white": danger,
-    "rounded-full": rounded,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-800": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-400": outline && warning,
-    "text-red-600": outline && danger,
-  });
+  const classes = classNames(
+    rest.className,
+    "flex items-center px-3 py-1.5 border",
+    {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-gray-900 bg-gray-900 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-yellow-400 bg-yellow-400 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-800": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-600": outline && danger,
+    }
+  );
 
-  return <button className={classes}>{children}</button>;
+  return (
+    <button className={classes} {...rest}>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
