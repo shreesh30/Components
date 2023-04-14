2e68ccb7851d5565372d6e6217f2c3893bec9bec
shreesh30 <shreesharya20@gmail.com>
2023-04-15 03:03:14 +0530

    button styling and svg added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded outline>
-          Click Here!
+        <Button secondary rounded outline>
+          <GoBell />
+          Click me!!
         </Button>
       </div>
-
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Buy Now!
         </Button>
       </div>
-
       <div>
-        <Button warning>See Deal</Button>
-      </div>
-
-      <div>
-        <Button secondary outline>
-          Hide Ads
+        <Button warning>
+          <GoDatabase />
+          See Deal!
         </Button>
       </div>
-
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
       <div>
         <Button primary rounded>
-          Something
+          Something!
         </Button>
       </div>
     </div>
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,3 @@
-// import PropTypes from "prop-types";
 import classNames from "classnames";
 
 function Button({
@@ -11,27 +10,35 @@ function Button({
   outline,
   rounded,
 }) {
-  const classes = classNames("px-3 py-1.5 border", {
+  const classes = classNames("flex items-center px-3 py-1.5 border", {
     "border-blue-500 bg-blue-500 text-white": primary,
     "border-gray-900 bg-gray-900 text-white": secondary,
     "border-green-500 bg-green-500 text-white": success,
     "border-yellow-400 bg-yellow-400 text-white": warning,
     "border-red-500 bg-red-500 text-white": danger,
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-800": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-400": outline && warning,
+    "text-red-600": outline && danger,
   });
+
   return <button className={classes}>{children}</button>;
 }
 
 Button.propTypes = {
-  checkVariation: ({ primary, secondary, success, warning, danger }) => {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
     const count =
       Number(!!primary) +
       Number(!!secondary) +
-      Number(!!success) +
       Number(!!warning) +
+      Number(!!success) +
       Number(!!danger);
 
     if (count > 1) {
-      throw new Error(
+      return new Error(
         "Only one of primary, secondary, success, warning, danger can be true"
       );
     }
