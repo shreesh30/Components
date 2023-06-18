7002e49e48a67e280aff763959aa43e3010b88e1
shreesh30 <shreesharya20@gmail.com>
2023-06-19 02:04:14 +0530

    reusable table completed

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Table from "./Table";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
@@ -8,7 +9,12 @@ function SortableTable(props) {
   const { config, data } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
@@ -28,8 +34,14 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          {column.label} IS SORTABLE
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => handleClick(column.label)}
+        >
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
         </th>
       ),
     };
@@ -53,12 +65,39 @@ function SortableTable(props) {
     });
   }
 
-  return (
-    <div>
-      {sortOrder}-{sortBy}
-      <Table {...props} config={updatedConfig} data={sortedData} />
-    </div>
-  );
+  return <Table {...props} config={updatedConfig} data={sortedData} />;
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoTriangleUp />
+        <GoTriangleDown />
+      </div>
+    );
+  }
+
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoTriangleUp />
+        <GoTriangleDown />
+      </div>
+    );
+  } else if (sortOrder === "asc") {
+    return (
+      <div>
+        <GoTriangleUp />
+      </div>
+    );
+  } else if (sortOrder === "desc") {
+    return (
+      <div>
+        <GoTriangleDown />
+      </div>
+    );
+  }
 }
 
 export default SortableTable;
